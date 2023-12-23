4976ff97f1e60d4b1f85b5a0499007319cccc2e7
Annerose Tartler <annerose.tartler@gmail.com>
2023-12-23 22:03:48 +0100

    satisfactory console output, diagram still missing

--- Female-Eponyms.py
+++ Female-Eponyms.py
@@ -8,40 +8,58 @@ raw_data = pd.read_csv('WomenPlantGenera_v1-3.csv')
 selected_data = raw_data[['id', 'genus-name', 'genus-wfo-status', 'family-name-wfo', 'eponymy-name', 'eponymy-yob', 'eponymy-yod', 'eponymy-occupation', 'eponymy-is-real', 'eponymy-country']]
 
 # gets the user input
-plant_name = input('Please enter the plant name you want to check:')
+plant_name = input('Please enter the plant name you want to check: ')
 
-# checks the existence of a plant_name in 'genus-name'
-def name_existence(plant_name):
-    name_exists = plant_name in selected_data['genus-name'].values
-    return name_exists
+# gives out a series of all the entries that have plant_name as genus-name
+connected_entries = selected_data[selected_data['genus-name']==plant_name]
 
+def plant_information(plant_name):
+    if connected_entries.empty:
+        print(f'The plant name "{plant_name}" does not exist in the selected data.')
+    else:
+        print(exist_information(plant_name))
+       
 # returns more information about the plant the user was searching for
-def more_information(plantname):
-    string = str("The plant...." + plantname) # Instead, this will take all the information about the plant and do something with it in the future
+def exist_information(plant_name):
+    string = str(f'The plant name "{plant_name}" exists in the selected data. It has {len(connected_entries)} results: \n') + combined_eponomy_information() + combined_exist_informations()
     return string
 
-# calls the function 'name_existence' with the plant of the user input
-plant_exists = name_existence(plant_name)
+def combined_eponomy_information():
+    if len(connected_entries) == 1:
+        return eponomy_information()
+    elif (connected_entries['genus-name'].nunique()==1 and connected_entries['eponymy-yob'].nunique()==1): # Hier könnte ich auch alle einzelnen angeben, wenns mich freut
+        return eponomy_information()
+    else:
+        return 'The different plants come from different women with the same name. To the first woman in the list, the following information is aviable: ' + eponomy_information() # Mögliche Erweiterung: tatsächlich einzeln nennen
+    
+def eponomy_information():
+    string = f'The flowers namesake is {connected_entries["eponymy-name"].iloc[0]}. She ' + reality_information()
+    return string
 
-# gives out if plant exists
-if plant_exists:
-    print(f'The plant name "{plant_name}" exists in the selected data. ' + more_information(plant_name))
-else:
-    print(f'The plant name "{plant_name}" does not exist in the selected data.')
+def reality_information():
+    if connected_entries['eponymy-is-real'].iloc[0]:
+        return f'was a {connected_entries["eponymy-occupation"].iloc[0]} and lived from {connected_entries["eponymy-yob"].iloc[0]} to {connected_entries["eponymy-yod"].iloc[0]} in {connected_entries["eponymy-country"].iloc[0]}. \n'
+    else:
+        return f'is a {connected_entries["eponymy-occupation"].iloc[0]}. \n'
 
+# sends the individual rows to indiv_exist_information(row_data) in order to be processed 
+def combined_exist_informations():
+    individual_rows = []
+    
+    for i, (index, row) in enumerate(connected_entries.iterrows(), start = 1):
+        row_info = indiv_exist_information(row, i)
+        individual_rows.append(row_info)
+    
+    string = '\n'.join(individual_rows)
+    
+    return string
 
+# evtl mit nummerierung durchschicken?
+def indiv_exist_information(row_data, number):
+    string = f'Entry number {number} belongs to the wfo-family of the {row_data["family-name-wfo"]}. Its wfo-Status is: {row_data["genus-wfo-status"]}'
+    return string
 
-# for debugging 
-# print('..........................')
-# print(selected_data.head())
-
-
-
-
-
-
-# Missing: LOOP!
+print(plant_information(plant_name))
 
 # diagram idea: are more plants named after her (needs: eponymy name, yob and yod)
 # diagram idea: if is-real = true => Country visualization
-
